895e0a1daf3d58e0641bd06a49351e516b28edeb
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-11-15 06:45:24 +0530

    Fix prettier check

--- src/animateDisplay.js
+++ src/animateDisplay.js
@@ -1,24 +1,24 @@
 export default function animatedDisplay(value, onClick) {
-    let count = 0;
-    onClick("");
-    let tempString = "";
-    let intervalID = setInterval(
-        () => {
-            const buttons = document.getElementsByTagName("button");
-            for (let i = 0; i < buttons.length; i++) {
-                buttons[i].classList.remove("red");
-                if (buttons[i].innerText === value[count]) {
-                    buttons[i].classList.add("red");
-                }
-            }
-            tempString += value[count];
-            onClick(tempString);
-            count++;
-            if (count >= value.length) {
-                clearInterval(intervalID);
-            }
-        },
-        1000,
-        count
-    );
+  let count = 0;
+  onClick("");
+  let tempString = "";
+  let intervalID = setInterval(
+    () => {
+      const buttons = document.getElementsByTagName("button");
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove("red");
+        if (buttons[i].innerText === value[count]) {
+          buttons[i].classList.add("red");
+        }
+      }
+      tempString += value[count];
+      onClick(tempString);
+      count++;
+      if (count >= value.length) {
+        clearInterval(intervalID);
+      }
+    },
+    1000,
+    count
+  );
 }
--- src/customComponents/Dialer.js
+++ src/customComponents/Dialer.js
@@ -3,13 +3,13 @@ import Display from "./Display";
 import DialPad from "./DialPad";
 
 export default function Dialer(props) {
-    const params = new URLSearchParams(window.location.search);
-    const [content, setContent] = useState(params.get("q"));
+  const params = new URLSearchParams(window.location.search);
+  const [content, setContent] = useState(params.get("q"));
 
-    return (
-        <>
-            <Display value={content} />
-            <DialPad onClick={setContent} value={content} />
-        </>
-    );
+  return (
+    <>
+      <Display value={content} />
+      <DialPad onClick={setContent} value={content} />
+    </>
+  );
 }
